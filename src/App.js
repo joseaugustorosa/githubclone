77e1a2b849a3b287cc210b098879b8fb67785eb6
José Augusto Nascimento Rosa <augusto.rosarj@gmail.com>
2024-03-24 21:30:59 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className='cabecalho'>
+          <img src='bars-solid.svg' className='toolbarHeader'></img>
+            <img className='logohub' src='github-mark-white.png' ></img>
+            <div className='nomeHeader'>
+              joseaugustorosa
+            </div>
+          
+           <div className='paipersonalHeader'>
+           <img className='prheader' src='pr.svg'></img>
+            <img  className = 'personalHeader' src='https://avatars.githubusercontent.com/u/101712645?v=4'></img>
+            </div>
+        </header>
+        <nav className='navBar'>
+              <ul >
+                <li className='itemNavbar'>
+         Overview
+                </li>
+                <li className='itemNavbar'>
+                  Repositories <span className='itemNavBarcOUNTER'>32</span>
+                  </li>
+                  <li className='itemNavbar'>
+                    Projects
+                  </li>
+                  <li className='itemNavbar'>
+                  Packages
+                  </li>
+                  <li className='itemNavbar'>
+                 Stars
+                  </li>
+              </ul>
+        </nav>
+        <div className='main'>
+
+        </div>
     </div>
   );
 }
